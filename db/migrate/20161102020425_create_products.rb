fe83e6bc7bc2c419738cb2eaaf0751de6f19e4d1
Jeremy Ens <jeremyens@gmail.com>
2016-11-01 21:39:58 -0500

    initial commit

---
+++ db/migrate/20161102020425_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :quantity
+      t.integer :category_id
+      t.boolean :is_active
+
+      t.timestamps
+    end
+  end
+end

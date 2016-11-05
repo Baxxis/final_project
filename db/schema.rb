946c7cb15f06c885a570d7702085271a5c1d3539
Jeremy Ens <jeremyens@gmail.com>
2016-11-05 18:40:57 -0500

    scaffold order_status and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105232812) do
+ActiveRecord::Schema.define(version: 20161105233532) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.boolean  "is_active"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end

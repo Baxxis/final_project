d7449b11504bf935e2004192b64e6dd9f52ec9a8
Jeremy Ens <jeremyens@gmail.com>
2016-11-26 13:38:46 -0600

    add pagination. Begin adding checkout process

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    @products = Product.all.order('name').page(params[:page]).per(5)
     @categories = Category.all
     @order_item = current_order.order_items.new
     retreive_products
